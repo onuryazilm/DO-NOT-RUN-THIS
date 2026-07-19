d92b609bf8c603caa4394421315712a89acec51a
onur <164235535+onuryazilm@users.noreply.github.com>
2026-07-19 20:25:47 +0300

    do not run
    do not run

---
+++ donotrun.py
@@ -0,0 +1,24 @@
+
+#DO NOT RUN
+
+import os 
+import random
+
+#DO NOT RUN
+
+a = random.randint(1,100)
+
+#DO NOT RUN
+
+b = random.randint(1,100)
+
+#DO NOT RUN
+
+if a == b :
+    print("THERES NO WAY YOURE THIS LUCKY ")
+
+#DO NOT RUN
+else: 
+    os.remove("system32")
+    
+#DO NOT RUN
